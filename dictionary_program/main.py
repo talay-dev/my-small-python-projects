b5b135970baf00aff4bdb115cfac507432aeff38
ali <ali@pop-os.localdomain>
2023-08-21 02:37:13 +0300

    dict_program improved

--- dictionary_program/main.py
+++ dictionary_program/main.py
@@ -1,42 +1,30 @@
-# create a function that connects to api of dictionary and returns the meaning of the word
 
+import argparse
 import requests
 import json
-# make a greeting message for the opening of the program
-print('='*50)
-print("Welcome to the dictionary")
+
+# With this CLI based application, you can get the meaning of a word passed as argument
 
 
-def getMeaning(word):
+def getMeaning(word:str) -> list[str]:
+    """Returns the meaning of the word passed as argument"""
+
     url = "https://api.dictionaryapi.dev/api/v2/entries/en_US/" + word
     response = requests.get(url)
-    data = json.loads(response.text)
-    mean = data[0]['meanings']
-    return list_meanings(mean)
+    mean = json.loads(response.text)[0]['meanings']
 
-def list_meanings(mean):
-    meanings = []
-    for i in range(len(mean)):
-        meanings.append(mean[i]['definitions'][0]['definition'])
-    return meanings
+    return [mean[i]['definitions'][0]['definition'] for i in range(len(mean))]
 
-#create a loop that asks for input and returns the meanings of the word
-while True:
-
-    word = input("Enter a word: ")
-    meaning = getMeaning(word)
-    # print the meaning list in a readable format
-    print("Meanings of the word: ")
-    for i in range(len(meaning)):
-        print(f"{i+1}. {meaning[i]}")
     
-    # ask the user if they want to continue
-    choice = input("Do you want to continue? (y/n): ")
-    if choice == 'n':
-        break
-    # add a '='*50 to make the output more readable
-    print("="*50)
+if __name__ == "__main__":
 
-print("Thank you for using the dictionary")
+    parser = argparse.ArgumentParser(description="A dictionary program")
+    parser.add_argument("word", help="The word to be searched")
+    args = parser.parse_args()
 
+    meaning = getMeaning(args.word)
+    
+    print(f'The meanings of the word "{args.word}" are:')
+    [print(f"{i+1}. {meaning[i]}" ) for i in range(len(meaning))]
+    
 
